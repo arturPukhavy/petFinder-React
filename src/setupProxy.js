9cdb02271276c6e6f71df3d26547a45fea3f8b3b
Artur Pukhavoi <arturpuhice@gmail.com>
2025-01-16 00:30:25 +0300

    add api/v1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/v1', // Proxy all requests starting with /api/v1
     createProxyMiddleware({
-      target: 'http://localhost:3000', // Backend server
+      target: 'http://localhost:3000/api/v1', // Backend server
       changeOrigin: true, // Adjust the origin header to match the target
       logLevel: 'debug', // Add logging to debug requests
     })
