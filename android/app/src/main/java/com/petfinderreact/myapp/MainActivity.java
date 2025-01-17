f397aa5ccbe2c57df3df95375c8fc684391cefd0
Artur Pukhavoi <arturpuhice@gmail.com>
2025-01-17 18:32:31 +0300

    add capacitor

---
+++ android/app/src/main/java/com/petfinderreact/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.petfinderreact.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.petfinderreact.myapp',
+  appName: 'petfinder-react',
+  webDir: 'build'
+};
+
+export default config;
